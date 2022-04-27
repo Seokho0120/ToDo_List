e9898397b6721d0a30c5f51a9156b6e95933707e
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2022-04-27 14:06:55 +0900

    [Add] Item 내용 수정

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -14,6 +14,36 @@ export default function Item({ id, done, text }) {
   );
 }
 
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #62bfed;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #62bfed;
+      color: #62bfed;
+    `}// 조건부 스타일링 css 사용
+`;
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
 const Remove = styled.div`
   display: flex;
   align-items: center;
@@ -35,36 +65,6 @@ const ItemBlock = styled.div`
   &:hover {
     ${Remove} {
       display: initial;
-    }
+    } // 컴포넌트 셀렉터 : Remove 컴포넌트를 보여줌
   }
-`;
-
-const CheckCircle = styled.div`
-  width: 32px;
-  height: 32px;
-  border-radius: 16px;
-  border: 1px solid #ced4da;
-  font-size: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 20px;
-  cursor: pointer;
-  ${(props) =>
-    props.done &&
-    css`
-      border: 1px solid #38d9a9;
-      color: #38d9a9;
-    `}
-`;
-
-const Text = styled.div`
-  flex: 1;
-  font-size: 21px;
-  color: #495057;
-  ${(props) =>
-    props.done &&
-    css`
-      color: #ced4da;
-    `}
 `;
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import Item from "./Item";
 
 export default function List() {
-  return <ListBlock>TodoList</ListBlock>;
+  return (
+    <ListBlock>
+      <Item text="이력서 피드백 예정" done={true} />
+      <Item text="ToDo_List 만들기" done={true} />
+      <Item text="면접 준비_리액트 질문" done={false} />
+      <Item text="면접 준비_자바스크립트 질문" done={false} />
+      <Item text="면접 준비_CS 질문" done={false} />
+    </ListBlock>
+  );
 }
 
 const ListBlock = styled.div`
